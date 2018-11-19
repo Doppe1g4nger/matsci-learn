9b6105920fa5aad0e391a5e3af58be3e287e25bb
Daniel Dopp <dbdopp@lbl.gov>
2018-11-19 13:41:30 -0800

    Making model more similar to tehrani version, but weird results?

--- examples/bulk_shear_prediction.py
+++ examples/bulk_shear_prediction.py
@@ -1,15 +1,17 @@
 from math import sqrt, floor
 from time import sleep
 
-from sklearn.preprocessing import scale, normalize
-from sklearn.model_selection import KFold, train_test_split
+from sklearn import preprocessing
+from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.svm import SVR
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 
-from matminer.datasets.convenience_loaders import load_tehrani_superhard_mat
+from matminer.utils.io import load_dataframe_from_json
+# from matminer.datasets.convenience_loaders import load_tehrani_superhard_mat
+
 from mslearn.featurization import AutoFeaturizer
 from mslearn.preprocessing import DataCleaner, FeatureReducer
 from mslearn.automl.adaptors import TPOTAdaptor
@@ -24,95 +26,137 @@ performance of our own model auto generated using the matsci-learn pipeline
 with features generated using the matminer software package.
 """
 
-# Suppress warnings for readability
-import warnings
-warnings.filterwarnings("ignore")
 
-# TEHRANI REPRODUCTION
-# Load in training data and separate out features from targets
-df = load_tehrani_superhard_mat(data="engineered_features")
+def load_tehrani_superhard_mat(data="all"):
+    if data not in {"all", "engineered_features", "basic_descriptors"}:
+        raise ValueError(f"Data description type {data} not recognized")
 
-target_labels = ["bulk_modulus", "shear_modulus"]
-features = df.drop(target_labels, axis=1).values
+    df = load_dataframe_from_json("tehrani_superhard_mat.json.gz")
 
-# Normalize and scale data to work better with SVM model
-features = normalize(features)
-features = scale(features)
+    if data == "engineered_features":
+        df = df.drop(["formula", "composition",
+                      "structure", "initial_structure",
+                      "material_id"], axis=1)
+    elif data == "basic_descriptors":
+        df = df.drop([thing for thing in df.columns if thing not in {
+            "composition", "structure", "bulk_modulus", "shear_modulus",
+        }], axis=1)
 
-# Define a cross validation scheme and model to test
-kfr = KFold(n_splits=10, shuffle=True, random_state=42)
-model = SVR(gamma=.01, C=10)
+    return df
 
-# For each target train a model and evaluate with cross validation
-for target_label in target_labels:
-    target = df[target_label].values
 
-    r2_scores = []
-    rmse_scores = []
-    prediction_list = []
-    target_list = []
-    for train_index, test_index in tqdm(
-            kfr.split(features, target),
-            desc="Cross validation progress for {}".format(target_label),
-            total=10):
-        train_features = features[train_index]
-        test_features = features[test_index]
+if __name__ == '__main__':
+    # TEHRANI REPRODUCTION
+    # Load in training data and separate out features from targets
+    df = load_tehrani_superhard_mat(data="engineered_features")
 
-        train_target = target[train_index]
-        test_target = target[test_index]
+    target_labels = ["bulk_modulus", "shear_modulus"]
+    # Convert to numpy array to work easier with sklearn
+    features = df.drop(target_labels, axis=1).values
 
-        model.fit(train_features, train_target)
-        prediction = model.predict(test_features)
-        r2_scores.append(r2_score(test_target, prediction))
-        rmse_scores.append(sqrt(mean_squared_error(test_target, prediction)))
+    # Define cross validation scheme, pre-processing pipeline and model to test
+    k_folds = KFold(n_splits=10, shuffle=True, random_state=42)
+    scaler = preprocessing.StandardScaler()
+    normalizer = preprocessing.Normalizer()
+    model = SVR(gamma=.01, C=10)
 
-        prediction_list += [item for item in prediction]
-        target_list += [item for item in test_target]
+    # For each target train a model and evaluate with cross validation
+    for target_label in target_labels:
+        # Convert to numpy array to work easier with sklearn
+        target = df[target_label].values
 
-    bounds = [0, max(target_list) + 20]
-    plt.scatter(target_list, prediction_list)
-    plt.title("{} actual vs. predicted".format(target_label))
-    plt.xlim(bounds)
-    plt.ylim(bounds)
-    x = np.linspace(0, bounds[1] + 20, 10000)
-    plt.plot(x, x, color="black")
-    plt.ylabel("Cross validated predictions for {}".format(target_label))
-    plt.xlabel("Actual values for {}".format(target_label))
-    plt.show()
+        # Hold onto these to calculate average performance
+        r2_scores = []
+        rmse_scores = []
+        # Hold onto these to graph predicted vs. actual
+        prediction_list = []
+        target_list = []
 
-    tqdm.write("RMSE for {}: {}".format(
-        target_label, sum(rmse_scores) / len(rmse_scores)
-    ))
-    tqdm.write("R^2 for {}: {}".format(
-        target_label, sum(r2_scores) / len(r2_scores)
-    ))
+        for train_index, test_index in tqdm(
+                k_folds.split(features, target),
+                desc="Cross validation progress for {}".format(target_label),
+                total=10
+        ):
+            train_features = features[train_index]
+            train_target = target[train_index]
 
-    sleep(1)
+            test_features = features[test_index]
+            test_target = target[test_index]
 
-# COMPARE TO MATBENCH
-df = load_tehrani_superhard_mat(data="basic_descriptors")
+            # Fit scaling on training data, don't see training data
+            scaler.fit(train_features)
+            train_features = scaler.transform(train_features)
+            test_features = scaler.transform(test_features)
 
-df = df.drop(["formula", "material_id", "shear_modulus",
-              "initial_structure"], axis=1)
-traindf = df.iloc[:floor(.8 * len(df))]
-testdf = df.iloc[floor(.8 * len(df)):]
-target = "bulk_modulus"
+            # Normalization is unique to each sample, doesn't need fit
+            train_features = normalizer.transform(train_features)
+            test_features = normalizer.transform(test_features)
 
-# Get top-level transformers
-autofeater = AutoFeaturizer()
-cleaner = DataCleaner()
-reducer = FeatureReducer()
-learner = TPOTAdaptor("regression", max_time_mins=5)
+            model.fit(train_features, train_target)
 
-# Fit transformers on training data
-traindf = autofeater.fit_transform(traindf, target)
-traindf = cleaner.fit_transform(traindf, target)
-traindf = reducer.fit_transform(traindf, target)
-learner.fit(traindf, target)
+            prediction = model.predict(test_features)
+            r2_scores.append(r2_score(test_target, prediction))
+            rmse_scores.append(sqrt(mean_squared_error(test_target,
+                                                       prediction)))
 
-# Apply the same transformations to the testing data
-testdf = autofeater.transform(testdf, target)
-testdf = cleaner.transform(testdf, target)
-testdf = reducer.transform(testdf, target)
-testdf = learner.predict(testdf, target)    #predict validation data
-print(testdf)
+            prediction_list += [item for item in prediction]
+            target_list += [item for item in test_target]
+
+        # Set up bounds for a pretty plot of prediction vs. ground truth
+        target_and_prediction = np.concatenate((prediction_list, target_list))
+        bounds = [min(target_and_prediction) - 20,
+                  max(target_and_prediction) + 20]
+        plt.xlim(bounds)
+        plt.ylim(bounds)
+
+        # Target on x and prediction on y means values lower than y=x
+        # are under-prediction and above are over-prediction
+        plt.scatter(target_list, prediction_list)
+
+        # Plot line representing perfect predictor
+        x = np.linspace(bounds[0], bounds[1], 1000)
+        plt.plot(x, x, color="black")
+
+        # Plot labels
+        plt.title("{} actual vs. predicted".format(target_label))
+        plt.ylabel("Cross validated predictions for {}".format(target_label))
+        plt.xlabel("Actual values for {}".format(target_label))
+
+        plt.show()
+
+        # Output average accuracies across CV runs
+        tqdm.write("RMSE for {}: {}".format(
+            target_label, sum(rmse_scores) / len(rmse_scores)
+        ))
+        tqdm.write("R^2 for {}: {}".format(
+            target_label, sum(r2_scores) / len(r2_scores)
+        ))
+
+        sleep(1)
+
+    # COMPARE TO MATBENCH
+    # df = load_tehrani_superhard_mat(data="basic_descriptors")
+    #
+    # df = df.drop(["shear_modulus"], axis=1)
+    # traindf = df.iloc[:floor(.8 * len(df))]
+    # testdf = df.iloc[floor(.8 * len(df)):]
+    # target = "bulk_modulus"
+    #
+    # # Get top-level transformers
+    # autofeater = AutoFeaturizer()
+    # cleaner = DataCleaner()
+    # reducer = FeatureReducer()
+    # learner = TPOTAdaptor("regression", max_time_mins=5)
+    #
+    # # Fit transformers on training data
+    # traindf = autofeater.fit_transform(traindf, target)
+    # traindf = cleaner.fit_transform(traindf, target)
+    # traindf = reducer.fit_transform(traindf, target)
+    # learner.fit(traindf, target)
+    #
+    # # Apply the same transformations to the testing data
+    # testdf = autofeater.transform(testdf, target)
+    # testdf = cleaner.transform(testdf, target)
+    # testdf = reducer.transform(testdf, target)
+    # testdf = learner.predict(testdf, target)    #predict validation data
+    # print(testdf)
